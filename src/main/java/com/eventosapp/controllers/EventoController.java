e13e7004e89658e65270ef5f358cf888b1938158
AlexAntonio <alexantonio308@gmail.com>
2020-12-14 13:17:39 -0300

    Banco com Adicionar e listar eventos

---
+++ src/main/java/com/eventosapp/controllers/EventoController.java
@@ -0,0 +1,38 @@
+package com.eventosapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.eventosapp.models.Evento;
+import com.eventosapp.repository.EventoRepository;
+
+@Controller
+public class EventoController {
+	
+	@Autowired
+	private EventoRepository er;
+	
+	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.GET)
+	public String form(){
+		return "evento/formEvento";
+	}
+
+	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.POST)
+	public String form(Evento evento){
+		
+		er.save(evento);
+		return "redirect:/cadastrarEvento";
+	}
+	
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("index");
+		Iterable<Evento> eventos = er.findAll();
+		mv.addObject("eventos", eventos);
+		return mv;
+	}
+
+}
---
+++ src/main/java/com/eventosapp/repository/EventoRepository.java
@@ -0,0 +1,10 @@
+package com.eventosapp.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.eventosapp.models.Evento;
+
+
+public interface EventoRepository extends CrudRepository <Evento, String>{
+
+}
