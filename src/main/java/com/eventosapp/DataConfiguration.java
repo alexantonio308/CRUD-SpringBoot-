dd411e8928d0e7e8ac6cb12b236f4535ec6e0926
AlexAntonio <alexantonio308@gmail.com>
2020-12-22 05:28:37 -0300

    Deploy heroku

--- src/main/java/com/eventosapp/DataConfiguration.java
+++ src/main/java/com/eventosapp/DataConfiguration.java
@@ -1,31 +1,39 @@
 package com.eventosapp;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import javax.sql.DataSource;
 
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
+@Profile("dev")
 public class DataConfiguration {
+	
+	@Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp?useTimezone=true&serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("alex14");
+        return dataSource;
+    }
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter(){
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+	}
 
-	 @Bean
-	    public BasicDataSource dataSource() throws URISyntaxException {
-	        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-	        String username = dbUri.getUserInfo().split(":")[0];
-	        String password = dbUri.getUserInfo().split(":")[1];
-	        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
-
-	        BasicDataSource basicDataSource = new BasicDataSource();
-	        basicDataSource.setUrl(dbUrl);
-	        basicDataSource.setUsername(username);
-	        basicDataSource.setPassword(password);
-
-	        return basicDataSource;
-	    }
 
 }
